9d683a6a28700e22839d32ce2458e08e625c51da
kapeksha <165630326+kapeksha@users.noreply.github.com>
2024-05-22 12:15:49 +0530

    Delete TaskManager directory

--- TaskManager/mytaskmanager/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Project, Task, TaskAssignment, Comment
-
-admin.site.register(Project)
-admin.site.register(Task)
-admin.site.register(TaskAssignment)
-admin.site.register(Comment)
