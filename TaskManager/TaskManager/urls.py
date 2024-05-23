fb35fe71fd9c04302e38b798da7169f048e7d97d
kapeksha <165630326+kapeksha@users.noreply.github.com>
2024-05-23 18:39:59 +0530

    Delete TaskManager directory

--- TaskManager/TaskManager/urls.py
+++
@@ -1,9 +0,0 @@
-
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),  # URL pattern for accessing Django admin site
-    path('api/', include('mytaskmanager.urls')),  # URL pattern for including API endpoints from mytaskmanager app
-]
--- TaskManager/mytaskmanager/tests.py
+++
@@ -1,6 +0,0 @@
-
-##imports TestCase class from django.test module.TestCase is base class for all Django test cases.
-
-from django.test import TestCase
-
-# Create your tests here.
